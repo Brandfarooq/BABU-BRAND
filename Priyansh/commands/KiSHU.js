adf2ad03f2b53abebe544810ddc834b78ee85fb4
Brandfarooq <idzmekr@gmail.com>
2025-07-03 23:12:21 +0530

    KiSHU.js

--- Priyansh/commands/KiSHU.js
+++ Priyansh/commands/KiSHU.js
@@ -1,13 +1,13 @@
 //@Prem-babu3608
 ////////////////////////////////////////////////////////
-/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
+/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BY FAROOQ  BABU
 const fs = require("fs");
 module.exports.config = {
 	name: "KISS-YOU",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "PREM BABU", ////////@prem-babu3608
-	description: "THIS BOT IS MR PREM SHARMA",
+	credits: "FAROOQ BABU", ////////@KHAN BRAND
+	description: "THIS BOT IS MR FAROOQ KHAN",
 	commandCategory: "no prefix",
     cooldowns: 5, 
 };
