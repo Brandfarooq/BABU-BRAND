671716676592423099d49f01eac2f5a02a2dbad0
Brandfarooq <idzmekr@gmail.com>
2025-07-03 23:05:13 +0530

    COUPLE-DP.js

--- Priyansh/commands/COUPLE-DP.js
+++ Priyansh/commands/COUPLE-DP.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "Coupledp",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "PREM BABU",
+  credits: "FAROOQ BABU",
   description: "couple Dp photos",
   commandCategory: "Random-IMG",
   usages: "couple dp",
