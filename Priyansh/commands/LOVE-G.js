82420399fc489298dd06fdeb2094b5b5bdfd0368
Brandfarooq <idzmekr@gmail.com>
2025-07-03 23:14:46 +0530

    LOVE-G.js

--- Priyansh/commands/LOVE-G.js
+++ Priyansh/commands/LOVE-G.js
@@ -1,13 +1,13 @@
-//@Prem-babu3608
+//@farooq-khan
 ////////////////////////////////////////////////////////
-/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
+/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT FAROOQ BABU
 const fs = require("fs");
 module.exports.config = {
 	name: "lOVE-YOU",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "PREM BABU", ////////////@prem-babu3608
-	description: "THIS BOT IS MR PREM SHARMA",
+	credits: "FAROOQ BABU", ////////////@farooq-khan
+	description: "THIS BOT IS MR FAROOQ KHAN",
 	commandCategory: "no prefix",
     cooldowns: 5, 
 };
